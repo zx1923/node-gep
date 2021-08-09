37d644c69d61db24051d81ac213bafff93da40a0
584574838@qq.com <584574838@qq.com>
2021-08-10 00:31:03 +0800

    Update chromosome.ts

--- src/gep/chromosome.ts
+++ src/gep/chromosome.ts
@@ -1,17 +1,17 @@
 import Gene from './gene';
 import { isArray, } from '../utils/helper';
-import { ChromosomeOption, OperItem, ChromosomeReduceFunc, DataInput } from '../types';
+import { ChromosomeOption, OperItem, ChromosomeReduceFunc, DataInput, ChromoGeneParts, GenePartItem } from '../types';
 import Activation from '../modules/activation';
 import Link from '../modules/link';
 
 class Chromosome {
-  private genesMap: Array<Gene>[]
+  private genesMap: Gene[][]
   shape: [number, number]
   linkFunc: typeof ChromosomeReduceFunc   // TODO: 连接函数，保留
   private lastShapeValue: Array<number>[]
   private activeFunc: Function
 
-  constructor(option: ChromosomeOption, chromoGeneSets: Array<Gene>[] = []) {
+  constructor(option: ChromosomeOption, chromoGeneSets: Gene[][] = []) {
     const { shape, linkFunc, activation } = option;
     this.shape = (shape && shape.length === 2) ? shape : [1, 1];
     this.linkFunc = linkFunc || Link.addUp;
@@ -26,14 +26,22 @@ class Chromosome {
   }
 
   /**
-   * 获取染色体基因组
+   * 获取染色体基因组编码结果
    * @returns 
    */
-  getGenes(): Array<OperItem>[] {
+  getEncodeGenes(): Array<OperItem>[] {
     const func = (row, col) => {
       return this.genesMap[row][col].encode();
     };
     return Chromosome.reduce(this.shape, func);
+  }
+
+  /**
+   * 获取基因
+   * @returns 
+   */
+  getGenes(): Array<Gene>[] {
+    return this.genesMap;
   }
 
   /**
@@ -69,12 +77,59 @@ class Chromosome {
   }
 
   /**
+   * 生成随机片段
+   * @returns 
+   */
+  getRandomParts() {
+    const parts: ChromoGeneParts = [];
+    const func = (row, col) => {
+      const genePart = this.genesMap[row][col].getRandomPart();
+      parts.push({
+        position: [row, col],
+        genePart,
+      });
+    };
+    Chromosome.reduce(this.shape, func);
+    return parts;
+  }
+
+  getMixedGenes(mixedParts: GenePartItem[]) {
+    // 1. 赋值当前的基因组
+    const func = (row, col) => {
+      const geneSets = [...this.genesMap[row][col].getGenes()];
+      return new Gene(geneSets);
+    };
+    const geneMapCopy: Gene[][] = Chromosome.reduce(this.shape, func);
+    // 2. 遍历碎片，准备混入
+    mixedParts.forEach(item => {
+      const [row, col] = item.position;
+      const { range, part } = item.genePart;
+      if (!geneMapCopy[row] || !geneMapCopy[row][col]) {
+        return;
+      }
+      // 3. 进行基因片段混合
+      const gns = geneMapCopy[row][col].getGenes();
+      const [start, end] = range;
+      let index = start;
+      while(index <= end) {
+        gns[index] = part[index - start];
+        index += 1;
+      }
+      // 4. 变异，更新计算树
+      geneMapCopy[row][col].mutate();
+      geneMapCopy[row][col].updateComputeTree();
+    });
+    // TODO: 基因 2d 转 gene 对象 2d
+    return geneMapCopy;
+  }
+
+  /**
    * 基于给定的shape，使用给定的回调函数进行循环归档
    * @param shape 形状
    * @param fn 回调
    * @returns 
    */
-  static reduce(shape: [number, number], fn: typeof ChromosomeReduceFunc): any[] {
+  static reduce(shape: [number, number], fn: typeof ChromosomeReduceFunc): any[][] {
     const [h, w] = shape;
     const res = []
     for (let i = 0; i < h; i++) {
